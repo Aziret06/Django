918fad4fa2f7a509e49365c1af431b8fc139efbd
Aziret06 <aziretkarimov06@gmail.com>
2024-08-22 00:40:07 +0600

    Static&media files, связdatabase Ñconnections

--- Django/urls.py
+++ Django/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from posts.views import (
@@ -28,4 +30,4 @@ urlpatterns = [
     path('', main_page),
     path('posts/', post_list_view),
     path('posts/<int:post_id>/', post_detail_view),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from posts.models import Post
 
-admin.site.register(Post)
+from posts.models import Post, Category, Tag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'rate')
+    list_editable = ('rate',)
+    list_display_links = ('title', 'category')
+    list_filter = ('category',)
+    search_fields = ('title', 'content')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
