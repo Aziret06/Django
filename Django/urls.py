b0f33738dbe7f7558c6d66330cf8b50c10538806
Aziret06 <aziretkarimov06@gmail.com>
2024-08-26 00:38:49 +0600

    users, authentication, registration, authorisation

--- Django/urls.py
+++ Django/urls.py
@@ -20,16 +20,25 @@ from django.contrib import admin
 from django.urls import path
 from posts.views import (
     text_answer,
-    main_page,
+    main_page_view,
     post_list_view,
     post_detail_view,
-    post_create_view)
+    post_create_view,
+)
+from user.views import (
+    register_view,
+    login_view,
+    logout_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('text_answer/', text_answer),
-    path('', main_page),
-    path('posts/', post_list_view),
-    path('posts/<int:post_id>/', post_detail_view),
-    path('posts/create/', post_create_view)
+    path('', main_page_view, name='main_page'),
+    path('posts/', post_list_view, name='post_list'),
+    path('posts/<int:post_id>/', post_detail_view, name='post_detail'),
+    path('posts/create/', post_create_view, name='post_create'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from posts.forms import PostForm
 from posts.models import Post
 
 
@@ -8,39 +11,44 @@ def text_answer(request):
         return HttpResponse('Введение в django, views')
 
 
-def main_page(request):
-    return render(request, 'main_page.html')
+def main_page_view(request):
+    if request.method == 'GET':
+        return render(request, 'main_page.html')
 
 
+@login_required(login_url='login')
 def post_list_view(request):
     if request.method == 'GET':
         posts = Post.objects.all()
         return render(request, 'post/post_list.html', context={'posts': posts})
 
 
+@login_required(login_url='login')
 def post_detail_view(request, post_id):
     if request.method == 'GET':
         post = Post.objects.get(id=post_id)
         return render(request, 'post/post_detail.html', context={'post': post})
 
 
+@login_required(login_url='login')
 def post_create_view(request):
 
     if request.method == 'GET':
-        return render(request, 'post/post_create.html')
+        form = PostForm()
+        return render(request, 'post/post_create.html', context={'form': form})
 
     elif request.method == 'POST':
 
-        image = request.FILES.get('image')
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        rate = request.POST.get('rate')
-
-        Post.objects.create(
-            image=image,
-            title=title,
-            content=content,
-            rate=rate
-        )
+        # image = request.FILES.get('image')
+        # title = request.POST.get('title')
+        # content = request.POST.get('content')
+        # rate = request.POST.get('rate')
+        #
+        # Post.objects.create(
+        #     image=image,
+        #     title=title,
+        #     content=content,
+        #     rate=rate
+        # )
 
         return redirect('/posts/')
